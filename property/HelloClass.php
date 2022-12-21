dd8f492d0b24771e1368651d2e6429f9d2ce7591
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-12-21 14:44:44 +0100

    two examples to understand what is property and methods

---
+++ property/HelloClass.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Define Class
+ * Define $content as Properties of Class
+ * Define hello() method in the class
+ */
+
+class HelloClass{
+    public $content;
+    function Hello(){
+        echo "Hello Class from the hello() method";
+    }
+}
+
+
+
+?>
---
+++ property/Posts.php
@@ -0,0 +1,31 @@
+<?php
+class Posts{
+    public $postContent;
+    public $postID;
+
+
+function addPost($data) : bool {
+    // Write Logic to Add the Posts to Database or File
+}
+
+// Update Records
+function updatePost($id,$data) : bool {
+    // Write Logic to Update the Posts to Database or File
+}
+
+// Delete Records
+function deletePost($id) : bool {
+    // Delete Posts
+}
+
+
+
+// Insert Records
+function fetchPost($id) : string {
+    // Fetch Posts
+}
+
+
+
+}
+?>
